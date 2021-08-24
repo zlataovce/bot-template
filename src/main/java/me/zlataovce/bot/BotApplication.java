a697eaf41ed2cc4cebf5c7af528529915c686c3b
zlataovce <34477304+zlataovce@users.noreply.github.com>
2021-08-24 08:42:40 +0200

    fix

--- src/main/java/me/zlataovce/bot/BotApplication.java
+++ src/main/java/me/zlataovce/bot/BotApplication.java
@@ -1,14 +1,21 @@
 package me.zlataovce.bot;
 
+import me.zlataovce.bot.services.DiscordClientProviderService;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Objects;
 
 @SpringBootApplication
 public class BotApplication {
 	public static void main(String[] args) {
-		new SpringApplicationBuilder()
+		ApplicationContext context = new SpringApplicationBuilder()
 				.sources(BotApplication.class)
 				.registerShutdownHook(true)
 				.run(args);
+		Objects.requireNonNull(
+				(DiscordClientProviderService) context.getBean("discordClientProvider")
+		).getGateway().onDisconnect().block();
 	}
 }
